5acd06541b43b5e1481ef9feedbb9136e5666c59
Sarah Park <sarahyaepark@gmail.com>
2020-12-11 14:15:17 -0800

    Updating database url

--- server/db/db.js
+++ server/db/db.js
@@ -3,9 +3,12 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  logging: false
-})
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:25060/${databaseName}`,
+  {
+    logging: false
+  }
+)
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
