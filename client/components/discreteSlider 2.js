bf0346f68e5352d26ace54ddfb5e78e14e28d9f9
Sarah Park <sarahyaepark@gmail.com>
2020-12-10 22:22:04 -0800

    Taking out prepare script

---
+++ client/components/discreteSlider 2.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Slider from '@material-ui/core/Slider'
+
+const useStyles = makeStyles({
+  root: {
+    width: 500
+  },
+  markLabel: {
+    fontSize: '2rem'
+  }
+})
+
+const marks = [
+  {
+    value: 0,
+    label: '😔'
+  },
+  {
+    value: 25,
+    label: '😕'
+  },
+  {
+    value: 50,
+    label: '😐'
+  },
+  {
+    value: 75,
+    label: '🙂'
+  },
+  {
+    value: 100,
+    label: '😊'
+  }
+]
+const valuetext = value => {
+  // do something
+  window.sessionStorage.setItem('currentMood', value)
+}
+export default function DiscreteSlider() {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <Slider
+        classes={{root: classes.root, markLabel: classes.markLabel}}
+        defaultValue={50}
+        getAriaValueText={valuetext}
+        aria-valuetext="hello"
+        step={25}
+        marks={marks}
+        min={0}
+        max={100}
+      />
+    </div>
+  )
+}
---
+++ client/store/dailyGoal 2.js
@@ -0,0 +1,80 @@
+import axios from 'axios'
+import history from '../history'
+
+const GET_GOAL = 'GET_GOAL'
+const UPDATE_GOAL = 'UPDATE_GOAL'
+
+const getGoal = goal => ({type: GET_GOAL, goal})
+const updatingGoal = goal => ({type: UPDATE_GOAL, goal})
+
+export const addGoal = (
+  userId,
+  description,
+  dailyEntryId
+) => async dispatch => {
+  let res
+  try {
+    res = await axios.post(`/api`, {
+      query: `mutation{addGoal(userId:${userId}, description: "${description}"),{description,dateCreated}}`
+    })
+  } catch (authError) {
+    return dispatch(getGoal({error: authError}))
+  }
+  try {
+    dispatch(getGoal(res.data.data.addGoal))
+    history.push('/home')
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
+export const updateGoal = (
+  userId,
+  goalId,
+  completed,
+  active,
+  dailyEntryId,
+  description
+) => async dispatch => {
+  let res
+  try {
+    if (description) {
+      res = await axios.post(`/api`, {
+        query: `mutation{updateGoal(id:${goalId}, description:"${description}"),{id,description,completed}}`
+      })
+    } else if (active !== undefined) {
+      res = await axios.post(`/api`, {
+        query: `mutation{updateGoal(id:${goalId}, active:${active}),{id,description,completed}}`
+      })
+    } else if (dailyEntryId !== undefined) {
+      res = await axios.post(`/api`, {
+        query: `mutation{updateGoal(id:${goalId}, dailyEntryId:${dailyEntryId}),{id,description,completed}}`
+      })
+    } else {
+      res = await axios.post(`/api`, {
+        query: `mutation{updateGoal(id:${goalId}, completed:${completed}),{id,description,completed}}`
+      })
+    }
+  } catch (authError) {
+    return dispatch(updatingGoal({error: authError}))
+  }
+  try {
+    dispatch(updatingGoal(res.data.data.updateGoal))
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = {}, action) {
+  switch (action.type) {
+    case GET_GOAL:
+      return action.goal
+    case UPDATE_GOAL:
+      return action.goal
+    default:
+      return state
+  }
+}
