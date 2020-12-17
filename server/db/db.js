6868c8fde2021489bd2428ff412cddb2a4728e74
Sarah Park <sarahyaepark@gmail.com>
2020-12-17 15:42:05 -0800

    Bug fix: Calendar loads with correct overview

--- server/db/db.js
+++ server/db/db.js
@@ -6,10 +6,10 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   // process.env.NODE_ENV === 'production'
   //   ?
-  `postgres://${process.env.USER}:${
-    process.env.PASSWORD
-  }@db-postgresql-sfo2-12541-do-user-8423247-0.b.db.ondigitalocean.com:25060/defaultdb?ssl=true`,
-  // : `postgres://localhost:5432/${databaseName}`,
+  // `postgres://${process.env.USER}:${
+  // process.env.PASSWORD
+  // }@db-postgresql-sfo2-12541-do-user-8423247-0.b.db.ondigitalocean.com:25060/defaultdb?ssl=true`,
+  `postgres://localhost:5432/${databaseName}`,
   {
     logging: false
   }
